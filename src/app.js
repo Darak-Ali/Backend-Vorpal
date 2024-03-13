54a99aca32a92ca4c63642b89139852b22bfd85d
Darak-Ali <darak26ali@gmail.com>
2024-03-13 15:10:27 +0500

    util code added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import  express  from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+const app=express();
+
 app.use(cors({
     origin:process.env.CORS_LINK,
     credentials:true
 }))
-app.use(express.json({limit:'16kb'}))
-app.use(express.urlencoded({limit:'16kb',extended:true}))
+app.use(express.json({limit:'16kb'}))//to accept json
+app.use(express.urlencoded({limit:'16kb',extended:true}))//to accept different types of url i.e example%20/example
 app.use(express.static("public"))
-app.use(cookieParser())
-const app=express();
-
+app.use(cookieParser())//for configuring cookies on user browser only which server can add or remove
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // require ('dotenv').config({path:"./env"})
 import dotenv from "dotenv"
 import connectdb from "./db/index.js"
-import app from "./app.js"
+import{ app }from "./app.js"
 dotenv.config({path:'./env'})
 connectdb()
 .then(()=>{
   try {
-    app.listen(process.env.PORT || 8000,()=>{console.log(`Server is running on ${process.env.PORT}`)})
-    
+    app.listen(process.env.PORT || 8000,()=>{
+      console.log(`Server is running on ${process.env.PORT}`)
+    })
   } catch (error) {
     console.log(`ErrorWhile running on port`,error);
     throw error
